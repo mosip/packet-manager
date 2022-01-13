271eed889be3c8b774156f7d74f672bd4a41060a
Monobikash Das <43202165+MonobikashDas@users.noreply.github.com>
2022-01-13 16:20:11 +0530

    Fixed auth issue (#50)
    Co-authored-by: Monobikash Das <M1045447@mindtree.com>
    Signed-off-by: Sowmya Ujjappa Banakar <sowmya.61022006@ltimindtree.com>

--- commons-packet/commons-packet-service/src/main/java/io/mosip/commons/packetmanager/dto/AuthorizedRolesDTO.java
+++ commons-packet/commons-packet-service/src/main/java/io/mosip/commons/packetmanager/dto/AuthorizedRolesDTO.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 
 
 @Component("authorizedRoles")
-@ConfigurationProperties(prefix = "mosip.role.packetmanager")
+@ConfigurationProperties(prefix = "mosip.role.commons-packet")
 @Getter
 @Setter
 public class AuthorizedRolesDTO {
