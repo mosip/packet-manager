93bcb45527738a8df4e3516518a3739dcf14cb7c
urviljoshi <urvvil08@gmail.com>
2022-05-11 11:26:42 +0530

    changed method name

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/util/ZipUtils.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/util/ZipUtils.java
@@ -61,7 +61,7 @@ public class ZipUtils {
 		return null;
 	}
 
-	static void extracted(Map<String, InputStream> checksumMap, Packet packet, List<String> allFileNames,
+	static void unzipAndValidateFiles (Map<String, InputStream> checksumMap, Packet packet, List<String> allFileNames,
 			List<String> notFoundFiles) throws IOException {
 		ByteArrayInputStream packetStream = new ByteArrayInputStream(packet.getPacket());
 		byte[] buffer = new byte[2048];
