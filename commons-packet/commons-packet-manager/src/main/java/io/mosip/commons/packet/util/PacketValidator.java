4e5e09d2113816a9df31d644f1aca65a47ea3e26
khuddus shariff <khuddusshariff0022@gmail.com>
2024-08-31 17:53:57 +0530

    Fix for Selected handels
    Signed-off-by: khuddus shariff <khuddusshariff0022@gmail.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/util/PacketValidator.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/util/PacketValidator.java
@@ -300,16 +300,26 @@ public class PacketValidator {
                 if (value != null) {
                     Object json = new JSONTokener(value).nextValue();
                     if (json instanceof org.json.JSONObject) {
-                        HashMap<String, Object> hashMap = new ObjectMapper().readValue(value, HashMap.class);
+                        HashMap<String, Object> hashMap = mapper.readValue(value, HashMap.class);
                         demographicIdentity.putIfAbsent(e.getKey(), hashMap);
                     }
+
                     else if (json instanceof JSONArray) {
                         List jsonList = new ArrayList<>();
+
                         JSONArray jsonArray = new JSONArray(value);
-                        for (int i = 0; i < jsonArray.length(); i++) {
-                            Object obj = jsonArray.get(i);
-                            HashMap<String, Object> hashMap = new ObjectMapper().readValue(obj.toString(), HashMap.class);
-                            jsonList.add(hashMap);
+                        if (jsonArray.length() > 0 && jsonArray.get(0) instanceof String) {
+                            List<String> stringList = new ArrayList<>();
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                stringList.add(jsonArray.getString(i));
+                            }
+                            demographicIdentity.putIfAbsent(e.getKey(), stringList);
+                        } else {
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                Object obj = jsonArray.get(i);
+                                HashMap<String, Object> hashMap = mapper.readValue(obj.toString(), HashMap.class);
+                                jsonList.add(hashMap);
+                            }
                         }
                         demographicIdentity.putIfAbsent(e.getKey(), jsonList);
                     } else
