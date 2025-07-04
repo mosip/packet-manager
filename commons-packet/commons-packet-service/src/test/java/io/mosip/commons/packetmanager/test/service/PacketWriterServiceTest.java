371aa59635d4a32a24bf2df3f633e6640cd77f3f
Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>
2025-07-04 17:59:13 +0530

    Added test cases
    Signed-off-by: Chetan Kumar Hirematha <chetankumar.h.239@gmail.com>

--- commons-packet/commons-packet-service/src/test/java/io/mosip/commons/packetmanager/test/service/PacketWriterServiceTest.java
+++ commons-packet/commons-packet-service/src/test/java/io/mosip/commons/packetmanager/test/service/PacketWriterServiceTest.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.mosip.commons.packet.dto.TagDeleteResponseDto;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -124,5 +125,173 @@ public class PacketWriterServiceTest {
 
 		packetWriterService.deleteTags(tagRequestDto);
 
+	}
+	/**
+	 * Tests updateTags with empty existing tags - should add new tags successfully
+	 */
+	@Test
+	public void testUpdateTags_WithEmptyExistingTags_AddsNewTagsSuccessfully() {
+		TagDto tagDto = new TagDto();
+		tagDto.setId("id");
+		Map<String, String> tags = new HashMap<>();
+		tags.put("newTag", "newValue");
+		tagDto.setTags(tags);
+
+		Mockito.when(packetReader.getTags(anyString())).thenReturn(new HashMap<>());
+		Mockito.when(packetWriter.addTags(any(), anyString())).thenReturn(tags);
+
+		TagResponseDto tagResponseDto = packetWriterService.updateTags(tagDto);
+		assertEquals(tagResponseDto.getTags(), tags);
+	}
+
+	/**
+	 * Tests updateTags with same value - should return same tags
+	 */
+	@Test
+	public void testUpdateTags_WithSameValue_ReturnsSameTags() {
+		TagDto tagDto = new TagDto();
+		tagDto.setId("id");
+		Map<String, String> tags = new HashMap<>();
+		tags.put("test", "testValue");
+		tagDto.setTags(tags);
+
+		TagResponseDto tagResponseDto = packetWriterService.updateTags(tagDto);
+		assertEquals(tagResponseDto.getTags(), tags);
+	}
+
+	/**
+	 * Tests updateTags with new tag - should add new tag successfully
+	 */
+	@Test
+	public void testUpdateTags_WithNewTag_AddsNewTagSuccessfully() {
+		TagDto tagDto = new TagDto();
+		tagDto.setId("id");
+		Map<String, String> tags = new HashMap<>();
+		tags.put("newTag", "newValue");
+		tagDto.setTags(tags);
+
+		Mockito.when(packetWriter.addTags(any(), anyString())).thenReturn(tags);
+
+		TagResponseDto tagResponseDto = packetWriterService.updateTags(tagDto);
+		assertEquals(tagResponseDto.getTags(), tags);
+	}
+
+	/**
+	 * Tests deleteTags with non-existent tag - should return success status
+	 */
+	@Test
+	public void testDeleteTags_WithNonExistentTag_ReturnsSuccessStatus() {
+		TagRequestDto tagRequestDto = new TagRequestDto();
+		tagRequestDto.setId("id");
+		List<String> tagNames = new ArrayList<>();
+		tagNames.add("nonExistentTag");
+		tagRequestDto.setTagNames(tagNames);
+
+		TagDeleteResponseDto result = packetWriterService.deleteTags(tagRequestDto);
+		assertEquals("Deleted Successfully", result.getStatus());
+	}
+
+	/**
+	 * Tests addTags when BaseCheckedException occurs - should throw TagCreationException
+	 */
+	@Test(expected = TagCreationException.class)
+	public void testAddTags_WhenBaseCheckedException_ThrowsTagCreationException() {
+		TagDto tagDto = new TagDto();
+		tagDto.setId("id");
+		Map<String, String> tags = new HashMap<>();
+		tags.put("testtag", "testValue");
+		tagDto.setTags(tags);
+
+		Mockito.when(packetReader.getTags(anyString())).thenReturn(new HashMap<>());
+		Mockito.when(packetWriter.addTags(any(), anyString()))
+				.thenThrow(new RuntimeException(new io.mosip.kernel.core.exception.BaseCheckedException("code", "message")));
+
+		packetWriterService.addTags(tagDto);
+	}
+
+	/**
+	 * Tests updateTags when BaseCheckedException occurs - should throw TagCreationException
+	 */
+	@Test(expected = TagCreationException.class)
+	public void testUpdateTags_WhenBaseCheckedException_ThrowsTagCreationException() {
+		TagDto tagDto = new TagDto();
+		tagDto.setId("id");
+		Map<String, String> tags = new HashMap<>();
+		tags.put("testtag", "testValue");
+		tagDto.setTags(tags);
+
+		Mockito.when(packetReader.getTags(anyString())).thenReturn(new HashMap<>());
+		Mockito.when(packetWriter.addTags(any(), anyString()))
+				.thenThrow(new RuntimeException(new io.mosip.kernel.core.exception.BaseCheckedException("code", "message")));
+
+		packetWriterService.updateTags(tagDto);
+	}
+
+	/**
+	 * Tests deleteTags when BaseCheckedException occurs - should throw TagDeletionException
+	 */
+	@Test(expected = TagDeletionException.class)
+	public void testDeleteTags_WhenBaseCheckedException_ThrowsTagDeletionException() {
+		TagRequestDto tagRequestDto = new TagRequestDto();
+		tagRequestDto.setId("id");
+		List<String> tagNames = new ArrayList<>();
+		tagNames.add("test");
+		tagRequestDto.setTagNames(tagNames);
+
+		Mockito.doThrow(new RuntimeException(new io.mosip.kernel.core.exception.BaseCheckedException("code", "message")))
+				.when(packetWriter).deleteTags(any(), anyString());
+
+		packetWriterService.deleteTags(tagRequestDto);
+	}
+
+	/**
+	 * Tests addTags when generic exception occurs - should throw TagCreationException
+	 */
+	@Test(expected = TagCreationException.class)
+	public void testAddTags_WhenGenericException_ThrowsTagCreationException() {
+		TagDto tagDto = new TagDto();
+		tagDto.setId("id");
+		Map<String, String> tags = new HashMap<>();
+		tags.put("testtag", "testValue");
+		tagDto.setTags(tags);
+
+		Mockito.when(packetWriter.addTags(any(), anyString()))
+				.thenThrow(new RuntimeException("Generic error"));
+
+		packetWriterService.addTags(tagDto);
+	}
+
+	/**
+	 * Tests updateTags when generic exception occurs - should throw TagCreationException
+	 */
+	@Test(expected = TagCreationException.class)
+	public void testUpdateTags_WhenGenericException_ThrowsTagCreationException() {
+		TagDto tagDto = new TagDto();
+		tagDto.setId("id");
+		Map<String, String> tags = new HashMap<>();
+		tags.put("testtag", "testValue");
+		tagDto.setTags(tags);
+
+		Mockito.when(packetWriter.addTags(any(), anyString()))
+				.thenThrow(new RuntimeException("Generic error"));
+
+		packetWriterService.updateTags(tagDto);
+	}
+
+	/**
+	 * Tests deleteTags when generic exception occurs - should throw TagDeletionException
+	 */
+	@Test(expected = TagDeletionException.class)
+	public void testDeleteTags_WhenGenericException_ThrowsTagDeletionException() {
+		TagRequestDto tagRequestDto = new TagRequestDto();
+		tagRequestDto.setId("id");
+		List<String> tagNames = new ArrayList<>();
+		tagNames.add("test");
+		tagRequestDto.setTagNames(tagNames);
+
+		Mockito.doThrow(new RuntimeException("Generic error"))
+				.when(packetWriter).deleteTags(any(), anyString());
+
+		packetWriterService.deleteTags(tagRequestDto);
 	}
 }
