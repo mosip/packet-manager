07ca6b6a807de1ef6e93effce68c91c8c0ca8a10
Ashok Kumar Sharma <ashok@mosip.io>
2025-08-17 21:07:54 +0530

    [MOSIP-42697] Added code for making the info cache configurable.
    Signed-off-by: Ashok Kumar Sharma <ashok@mosip.io>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/util/PropertyUtils.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/util/PropertyUtils.java
@@ -8,7 +8,24 @@ import org.springframework.stereotype.Component;
 public class PropertyUtils {
 
     private static final Logger LOGGER = PacketManagerLogger.getLogger(PropertyUtils.class);
-
+    /*
+     * Note on enabling info cache:
+     *
+     * Previously, issues were observed when the info cache was not cleared
+     * while processing a BIOMETRIC_CORRECTION packet for a given RID, since
+     * the correction packet is uploaded with the same RID.
+     *
+     * In production, we can enable the info cache for performance improvements,
+     * provided the following assumptions hold true:
+     *
+     * 1. By the time a BIOMETRIC_CORRECTION packet is uploaded, the info cache
+     *    for that RID will already be cleared. This is because, in production,
+     *    BIOMETRIC_CORRECTION packets are typically uploaded after some time,
+     *    and the cache duration is relatively short — reducing the risk of stale data.
+     *
+     * 2. Packet data is not modified "in-flight" (i.e., during processing),
+     *    ensuring that caching will not introduce inconsistencies.
+     */
     @Value("${packetmanager.cache.info.enabled:false}")
     private boolean isInfoCacheEnabled;
 
