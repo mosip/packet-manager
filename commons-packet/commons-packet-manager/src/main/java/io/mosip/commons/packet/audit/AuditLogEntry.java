c8273e36c2911ed87f34dd9d8ffc8fa51a14268b
kameshsr <kameshsr1338@gmail.com>
2025-08-28 11:26:20 +0530

    MOSIP-42842 Corrected version and fixed audit setActionTimeStamp
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/audit/AuditLogEntry.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/audit/AuditLogEntry.java
@@ -57,7 +57,7 @@ public class AuditLogEntry {
 
 			auditRequestDto = new AuditRequestDto();
 			auditRequestDto.setDescription(description);
-			auditRequestDto.setActionTimeStamp(DateUtils.getUTCCurrentDateTimeString());
+			auditRequestDto.setActionTimeStamp(DateUtils.getUTCCurrentDateTime());
 			auditRequestDto.setApplicationId(LoggerFileConstant.MOSIP_4.toString());
 			auditRequestDto.setApplicationName(LoggerFileConstant.PACKET_MANAGER.toString());
 			auditRequestDto.setCreatedBy(LoggerFileConstant.SYSTEM.toString());
