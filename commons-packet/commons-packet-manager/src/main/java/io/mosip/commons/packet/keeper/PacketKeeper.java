8f45b8daf384258101a303d1e0c8faa33e8b9504
Dhanendra Sahu <dhanendra.tech@gmail.com>
2026-03-01 14:55:55 +0530

    Fix the cache issue
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
@@ -140,7 +140,7 @@ public class PacketKeeper {
      * @param packetInfo : packet info
      * @return : Packet
      */
-    @Cacheable(value = "rawPackets", key = "#packetInfo.id + '_' + #packetInfo.packetName + '_' + #packetInfo.source + '_' + #packetInfo.process")
+    @Cacheable(value = "rawPackets", key = "#a0.id + '_' + #a0.packetName + '_' + #a0.source + '_' + #a0.process", condition = "#a0 != null")
     public Packet getPacket(PacketInfo packetInfo) throws PacketKeeperException {
         String packetName = getName(packetInfo.getId(), packetInfo.getPacketName());
         try (InputStream is = getAdapter().getObject(PACKET_MANAGER_ACCOUNT, packetInfo.getId(),
