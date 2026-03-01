51e5f9b341a1d540cc604a75f2f83f68b0dab881
Dhanendra Sahu <dhanendra.tech@gmail.com>
2026-03-01 16:38:05 +0530

    removed cache
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import io.mosip.commons.packet.exception.ObjectDoesnotExistsException;
-import org.springframework.cache.annotation.Cacheable;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -140,7 +139,6 @@ public class PacketKeeper {
      * @param packetInfo : packet info
      * @return : Packet
      */
-    @Cacheable(value = "rawPackets", key = "#a0.id + '_' + #a0.packetName + '_' + #a0.source + '_' + #a0.process", condition = "#a0 != null")
     public Packet getPacket(PacketInfo packetInfo) throws PacketKeeperException {
         String packetName = getName(packetInfo.getId(), packetInfo.getPacketName());
         try (InputStream is = getAdapter().getObject(PACKET_MANAGER_ACCOUNT, packetInfo.getId(),
