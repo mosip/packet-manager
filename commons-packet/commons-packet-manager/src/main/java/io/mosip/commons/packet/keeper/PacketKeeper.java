dc479f9e6e893e93d50841f5f617b33653978997
Dhanendra Sahu <dhanendra.tech@gmail.com>
2026-03-01 13:59:02 +0530

    Fix the /biometric endpoint issue taking longer time to respond
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
@@ -11,6 +11,7 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import io.mosip.commons.khazana.dto.ObjectDto;
@@ -139,6 +140,7 @@ public class PacketKeeper {
      * @param packetInfo : packet info
      * @return : Packet
      */
+    @Cacheable(value = "rawPackets", key = "#packetInfo.id + '_' + #packetInfo.packetName + '_' + #packetInfo.source + '_' + #packetInfo.process")
     public Packet getPacket(PacketInfo packetInfo) throws PacketKeeperException {
         String packetName = getName(packetInfo.getId(), packetInfo.getPacketName());
         try (InputStream is = getAdapter().getObject(PACKET_MANAGER_ACCOUNT, packetInfo.getId(),
