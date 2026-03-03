d4063e59cffc44759971d11154aa7109900d9202
Dhanendra Sahu <dhanendra.tech@gmail.com>
2026-03-03 23:54:31 +0530

    increased the performance of /info endpoint
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/keeper/PacketKeeper.java
@@ -11,7 +11,6 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import io.mosip.commons.khazana.dto.ObjectDto;
@@ -140,7 +139,6 @@ public class PacketKeeper {
      * @param packetInfo : packet info
      * @return : Packet
      */
-    @Cacheable(value = "packets", key = "'zip-'.concat(#packetInfo.id).concat('-').concat(#packetInfo.packetName).concat('-').concat(#packetInfo.source).concat('-').concat(#packetInfo.process)", unless = "#result == null")
     public Packet getPacket(PacketInfo packetInfo) throws PacketKeeperException {
         String packetName = getName(packetInfo.getId(), packetInfo.getPacketName());
         try (InputStream is = getAdapter().getObject(PACKET_MANAGER_ACCOUNT, packetInfo.getId(),
