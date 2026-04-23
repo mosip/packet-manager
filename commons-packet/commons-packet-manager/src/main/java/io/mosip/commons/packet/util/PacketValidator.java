8caf22592508ac7ce8a2cd975e3212ea4a292ad2
kameshsr <kameshsr1338@gmail.com>
2026-04-23 17:42:52 +0530

    MOSIP-44780 corrected exception
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/util/PacketValidator.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/util/PacketValidator.java
@@ -216,7 +216,7 @@ public class PacketValidator {
             }
             if (cause instanceof BaseUncheckedException ex) {
                 System.out.println("inside base unchecked exception");
-                throw new GetAllIdentityException(ex.getErrorCode());
+                throw new GetAllIdentityException(ex.getErrorCode(), (String) null);
             }
             throw new GetAllIdentityException(cause.getMessage());
 
