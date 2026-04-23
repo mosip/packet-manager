fc52ffc03664dfca0dc3d6bf40a44bdebe11e30b
kameshsr <kameshsr1338@gmail.com>
2026-04-23 17:29:25 +0530

    MOSIP-44780 added log
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/util/PacketValidator.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/util/PacketValidator.java
@@ -216,7 +216,7 @@ public class PacketValidator {
             }
             if (cause instanceof BaseUncheckedException ex) {
                 System.out.println("inside base unchecked exception");
-                throw new GetAllIdentityException(ex.getErrorCode(), ex.getErrorText());
+                throw new GetAllIdentityException(ex.getErrorCode());
             }
             throw new GetAllIdentityException(cause.getMessage());
 
