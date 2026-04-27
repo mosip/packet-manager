0ae11ab59083d90e97c8f4a6b1e9b7586ff03a9c
kameshsr <kameshsr1338@gmail.com>
2026-04-27 11:57:17 +0530

    MOSIP-44780 added npe
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/facade/PacketReader.java
+++ commons-packet/commons-packet-manager/src/main/java/io/mosip/commons/packet/facade/PacketReader.java
@@ -81,10 +81,18 @@ public class PacketReader {
         if (bypassCache)
             value = getProvider(source, process).getField(id, field, source, process);
         else {
-            Map debugMap = getAllFields(id, source, process);
-            System.out.println("debugMap : " + debugMap);
-            Optional<Object> optionalValue = getAllFields(id, source, process).entrySet().stream().filter(m-> m.getKey().equalsIgnoreCase(field) && m.getValue()!=null).map(m -> m.getValue()).findAny();
-            value = optionalValue.isPresent() ? optionalValue.get().toString() : null;
+//            Optional<Object> optionalValue = getAllFields(id, source, process).entrySet().stream().filter(m-> m.getKey().equalsIgnoreCase(field) && m.getValue()!=null).map(m -> m.getValue()).findAny();
+//            value = optionalValue.isPresent() ? optionalValue.get().toString() : null;
+
+            Optional<Object> optionalValue = getAllFields(id, source, process)
+                    .entrySet()
+                    .stream()
+                    .filter(m -> m.getKey().equalsIgnoreCase(field)) // ❌ null filter hata diya
+                    .map(m -> m.getValue())
+                    .findAny();
+
+// Direct get + toString (unsafe)
+            value = optionalValue.get().toString();
         }
         return value;
     }
